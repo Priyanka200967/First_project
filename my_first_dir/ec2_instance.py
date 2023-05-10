42647df07bcb6a3f781c2a55b53830c5e31b53e0
Priyanka200967 <132727376+Priyanka200967@users.noreply.github.com>
2023-05-10 11:49:16 +0530

    Update ec2_instance.py

--- my_first_dir/ec2_instance.py
+++ my_first_dir/ec2_instance.py
@@ -1,13 +1,1 @@
-import boto3
-
-
-ec2 = boto3.client('ec2',
-                    'us-east-1',
-                    aws_access_key_id= 'AKIA2YFZYNUJXL7ZG34D',
-                    aws_secret_access_key= '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')
-
-conn = ec2.run_instances(InstanceType='t2.micro',
-                        MaxCount=1,
-                        MinCount=1,
-                        ImageId='ami-0889a44b331db0194',
-                        KeyName='test1')
+print ("hi")
